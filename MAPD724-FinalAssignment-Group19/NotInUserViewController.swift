4f4ca78ce72be68edb72bf3c7536515eebb86f67
Murtaza0726 <murtazanaqvi0726@yahoo.com>
2023-04-23 13:42:25 -0400

    final submission-5

---
+++ MAPD724-FinalAssignment-Group19/NotInUserViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NotInUserViewController.swift
+//  MAPD724-FinalAssignment-Group19
+//
+//  Created by Murtaza Haider Naqvi on 2023-04-16.
+//
+
+import UIKit
+
+class NotInUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
